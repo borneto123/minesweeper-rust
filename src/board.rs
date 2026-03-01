bb6572600b26e605b7dfa39949fde52139b98d81
Borneto <bornaflinta2005@gmail.com>
2026-03-01 02:18:27 +0100

    Started implementing Board struct - added BoardIter (Not tested) - added get_tile() (Not tested)

--- src/board.rs
+++ src/board.rs
@@ -1,10 +1,55 @@
-use crate::tile::Tile;
+use crate::coords::Coords;
+use crate::tile::{Tile, TileContent};
 use crate::dimensions::Dimensions;
 
 
 pub struct Board {
-    dimension: Dimensions,
-    cells : Vec<Tile>,
+    dimensions: Dimensions,
+    config: BoardConfig,
+    tiles : Vec<Tile>,
+}
+
+pub struct BoardConfig{
+    dimensions: Dimensions,
+    mine_count: u64,
+}
+
+impl Board {
+    /* 
+    pub fn new(config: BoardConfig) -> Self {
+        // Filling all tiles with HiddenEmpty(0)
+        let tiles = vec![Tile::Hidden(TileContent::Empty(0)); config.dimensions.area()];
+
+    }
+*/
+    pub fn iter(&self) -> BoardIter<'_> {
+        BoardIter {
+            board: &self,
+            index: 0,
+        }
+    }
+
+    pub fn get_tile(&self, coords: &Coords) -> Option<&Tile> {
+        let index = coords.to_index(&self.dimensions).ok()?;
+        Some(&self.tiles[index])
+    }
 }
 
 
+pub struct BoardIter< 'a>{
+    board: &'a Board,
+    index: usize,
+}
+
+impl <'a> Iterator for BoardIter<'a> {
+    type Item = (Coords, &'a Tile);
+
+    fn next(&mut self) -> Option<Self::Item> {
+
+       // Returns None if out of bounds
+        let coords = Coords::from_index(&self.index, &self.board.dimensions).ok()?;
+        let old_index = self.index;
+        self.index += 1;
+        Some((coords, &self.board.tiles[old_index]))
+    }
+}
--- src/tile.rs
+++ src/tile.rs
@@ -1,9 +1,10 @@
+#[derive(Debug, Clone)]
 pub enum Tile {
     Hidden(TileContent),
     Flagged(TileContent),
     Revealed(TileContent)
 }
-
+#[derive(Debug, Clone)]
 pub enum TileContent {
     Mine,
     Empty(u8),
