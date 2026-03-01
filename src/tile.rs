cbc98192d031f0569479a7cab4080aec35c0203d
Borneto <bornaflinta2005@gmail.com>
2026-03-01 20:52:33 +0100

    Add tile helpers - is_mine() - update() - set_content() - place_mine()

--- src/tile.rs
+++ src/tile.rs
@@ -9,6 +9,46 @@ pub enum TileContent {
     Mine,
     Empty(u8),
 }
+#[derive(Debug, Clone)]
+
+pub enum TileError {
+    AlreadyHasMine,
+}
+
+
+
+impl Tile {
+    pub fn is_mine(&self) -> bool {
+        matches!(self,
+            Tile::Hidden(TileContent::Mine)
+            | Tile::Flagged(TileContent::Mine)
+            | Tile::Revealed(TileContent::Mine)
+        )
+    }
+
+    pub fn update(&mut self, new_state: Tile) {
+        *self = new_state;
+    }
+
+    pub fn set_content(&mut self, new_content: TileContent) {
+        match self {
+            Tile::Flagged(c)
+            | Tile::Revealed(c)
+            | Tile::Hidden(c) 
+            => *c = new_content
+        }
+    }
+
+    pub fn place_mine(&mut self) -> Result<(), TileError> {
+        if self.is_mine() {
+            return  Err(TileError::AlreadyHasMine);
+        }
+
+        self.set_content(TileContent::Mine);
+        Ok(())
+    }
+}
+
 
 impl Default for Tile {
     fn default() -> Self {
