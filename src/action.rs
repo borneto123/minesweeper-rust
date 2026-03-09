efd228f3d7f5e5a8396efa0cd692513a00f24e69
borneto <bornaflinta2005@gmail.com>
2026-03-09 17:45:03 +0100

    Refactor ActionErrors

--- src/action.rs
+++ src/action.rs
@@ -1,26 +1,30 @@
 use crate::{board::BoardConfig, coords::Coords, tile::TileRevealError};
-
+#[derive(Debug)]
 pub enum Action {
     StartGame {board_config: BoardConfig},
     FlagTile {coords: Coords},
     RevealTile {coords: Coords}
 }
+#[derive(Debug)]
 
 pub enum RevealTileResult {
     MineRevealed,
     TileRevealed(SafeTile)
 }
+#[derive(Debug)]
 
 pub enum SafeTile {
     Normal(Coords),
     Flood(Vec<Coords>),
     Last(Coords),
 }
+#[derive(Debug)]
 
 pub enum ActionRevealError {
     InvalidCoordinate,
     AlreadyRevealed,
     Flagged,
+    CoulndtAccesTile,
     Unknown,
 }
 
